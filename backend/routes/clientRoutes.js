9874d09aef1b45744cd3137250a5051ab5f846b3
Irya Patni <irya.patni04@gmail.com>
2025-12-20 01:54:06 +0530

    fix: remove file upload middleware for stability

--- backend/routes/clientRoutes.js
+++ backend/routes/clientRoutes.js
@@ -1,10 +1,9 @@
 import express from "express";
-import upload from "../config/cloudinary.js";
 import { addClient, getClients } from "../controllers/clientController.js";
 
 const router = express.Router();
 
-router.post("/", upload.single("image"), addClient);
+router.post("/", addClient);
 router.get("/", getClients);
 
 export default router;
