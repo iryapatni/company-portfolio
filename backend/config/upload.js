704c1c1ffe3e56eff01f49d4a9ab19f804b0448a
Irya Patni <irya.patni04@gmail.com>
2025-12-20 01:46:36 +0530

    fix: remove cloudinary storage and use memory upload

--- backend/config/upload.js
+++ backend/config/upload.js
@@ -1,17 +1,7 @@
 import multer from "multer";
-import { CloudinaryStorage } from "multer-storage-cloudinary";
-import cloudinary from "./cloudinary.js";
 
-const storage = new CloudinaryStorage({
-  cloudinary,
-  params: async (req, file) => {
-    return {
-      folder: "company-portfolio",
-      format: file.mimetype.split("/")[1], // jpg / png
-    };
-  },
+const upload = multer({
+  storage: multer.memoryStorage(),
 });
-
-const upload = multer({ storage });
 
 export default upload;
